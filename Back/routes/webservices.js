2683e540d9986a0953d80d55e9b4aee912e63cd7
Joseph-Emmanuel <josephE.banzio@gmail.com>
2019-08-28 10:04:35 +0200

    minor issues fixed

--- Back/routes/webservices.js
+++ Back/routes/webservices.js
@@ -1,9 +1,8 @@
 const MongoClient = require('mongodb').MongoClient
       bcrypt = require('bcryptjs')
       uri = "mongodb+srv://ghaust:EW63BjZ6FFBv5L6X@web-project-nvcrn.mongodb.net/test?retryWrites=true&w=majority";
-      client = new MongoClient(uri, { useNewUrlParser: true })
-      collection = client.db("Esiea").collection("Students")
-let Student = require('./Student')
+      
+//let Student = require('./Student')
 var express = require('express')
     app = express()
 
@@ -13,6 +12,11 @@ var express = require('express')
         next()
       })
 
+var server = app.listen(8081, "127.0.0.1",function () {
+      host = server.address().address
+      port = server.address().port
+      console.log("\nStudentDashboard listening at http://%s:%s\n", host, port)
+  })
 
 /* Première route /authentification
 On récupère un email et un mdp dans l'URL
@@ -21,33 +25,29 @@ alors on vérifie le mdp si c bon on retourne un message
 */
 
 app.get('/authentification/:email/:password', function(req, res){
+  const client = new MongoClient(uri, { useNewUrlParser: true })
   client.connect(err => {
     const collection = client.db("Esiea").collection("Students")  
     console.log('Authentification request')
     var umail = req.params.email
         query = { email: umail }
         upwd = req.params.password
-    
+    //Code d'erreur à la place du texte
+  
     collection.find(query).toArray(function(err, result){
         if (err) throw err
-        if(result == null)
-          result.send("Email not found!") //on retourne un message d'erreur
-        else
+        if(result.length < 1)
+          res.send("Email not found!") //on retourne un message d'erreur res.send(
+        else{
+          console.log("Email verification")
           var dbpwd = result[0].password
-
-        if(upwd == dbpwd)
-            result.send("Successfully connected!")       
+          console.log("Password verification")
+          if(upwd == dbpwd)
+              res.send("Successfully connected!")
+          else
+              res.send("Wrong password")
+          }
     })
   client.close();
 });
 })
-
-//serveur ici pour le moment 
-var server = app.listen(8081, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-  
-    console.log("App listening at http://%s:%s", host, port)
-  
-  })
