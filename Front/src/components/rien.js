ad9cc85ba903f5930138967cb49d4ade4b0ed65e
Robert <robert.bui@outlook.fr>
2019-08-29 13:38:12 +0200

    front version

--- Front/src/components/rien.js
+++ Front/src/components/rien.js
@@ -19,13 +19,6 @@ import CardContent from '@material-ui/core/CardContent';
 import Divider from '@material-ui/core/Divider';
 import MailIcon from '@material-ui/icons/Mail';
 import { withRouter } from 'react-router';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -38,7 +31,7 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
   },
   card: {
-    maxWidth: 275,
+    minWidth: 275,
   },
   bullet: {
     display: 'inline-block',
@@ -54,27 +47,9 @@ const useStyles = makeStyles(theme => ({
   fullList: {
     width: 'auto',
   },
-    media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
 }));
 
-const rows = [
-  { matiere:'Matière 1', bloc:'Bloc 1', note:10},
-  { matiere:'Matière 2', bloc:'Bloc 2', note:10},
-  { matiere:'Matière 3', bloc:'Bloc 2', note:10},
-  { matiere:'Matière 4', bloc:'Bloc 3', note:10},
-  { matiere:'Matière 5', bloc:'Bloc 2', note:10},
-  { matiere:'Matière 6', bloc:'Bloc 2', note:10},
-  { matiere:'Matière 7', bloc:'Bloc 1', note:10},
-  { matiere:'Matière 8', bloc:'Bloc 2', note:10},
-]
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center'
-};
 
 export default function ButtonAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -101,7 +76,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Student DashBoard
+            Lourd
           </Typography>
           <Button color="inherit" onClick={handleClick}>Login</Button>
           <Menu
@@ -120,80 +95,31 @@ export default function ButtonAppBar() {
         </Toolbar>
       </AppBar>
 
-      <div style={{display:'flex',alignItems:'center'}}>
-
-        <CardMedia
-        
-          image="./images/bob.png"
-          title="bob"
-        />
-        
-      <Card className={classes.card} >
-        <CardContent>
-
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            4ème année
-          </Typography>
-          <Typography variant="h5" component="h2">
-          Prénom Nom
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            Filières
-          </Typography>
-          <Typography variant="body2" component="p">
-            Adresse
-            <br />
-            adresse@email.fr
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <Card className={classes.card} >
+      <Card className={classes.card}>
         <CardContent>
           <Typography className={classes.title} color="textSecondary" gutterBottom>
-            4ème année
+            Word of the Day
           </Typography>
           <Typography variant="h5" component="h2">
-          Prénom Nom
+            be
+            {bull}
+            nev
+            {bull}o{bull}
+            lent
           </Typography>
           <Typography className={classes.pos} color="textSecondary">
-            Filières
+            adjective
           </Typography>
           <Typography variant="body2" component="p">
-            Adresse
+            well meaning and kindly.
             <br />
-            adresse@email.fr
+            {'"a benevolent smile"'}
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
       </Card>
-
-
-
-      </div>
-
-      <Paper>
-        <Table minWidth="650">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Matière</TableCell>
-              <TableCell align="left">Bloc</TableCell>
-              <TableCell align="left">Note</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.matiere}>
-                <TableCell align="left">
-                  {row.matiere}
-                </TableCell>
-                <TableCell align="left">{row.bloc}</TableCell>
-                <TableCell align="left">{row.note}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-
     </div>
   );
 }
